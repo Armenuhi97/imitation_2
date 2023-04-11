fc7bbeee2d4707ddbc13466664e647b5357de055
armenuhi0997 <armenuhimanukyan@annaniks.com>
2023-04-11 10:26:55 +0400

    finish design withdraw

--- src/app/modules/withdraw/components/full-withdraw/full-withdraw.component.ts
+++ src/app/modules/withdraw/components/full-withdraw/full-withdraw.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-full-withdraw',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./full-withdraw.component.scss']
 })
 export class FullWithdrawComponent {
+  withdrawGroup: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.withdrawGroup = this.initGroup();
+  }
+
+  private initGroup(): FormGroup {
+    return this.fb.group({
+      available_amount: [null, Validators.required],
+      withdrawal_amount: [null, Validators.required],
+      password: [null, Validators.required]
+    })
+  }
 }
--- src/app/modules/withdraw/components/withdraw/withdraw.component.ts
+++ src/app/modules/withdraw/components/withdraw/withdraw.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-withdraw',
   templateUrl: './withdraw.component.html',
-  styleUrls: ['./withdraw.component.scss']
+  styleUrls: ['./withdraw.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class WithdrawComponent {
-  isFull = true;
+  isFull = false;
 }
